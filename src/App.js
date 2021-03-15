b53074d33880cda0255ef8cac81e3d65fa0bdb8f
Alisson Rodrigues <48796830+Wander27Rodrigues@users.noreply.github.com>
2021-03-14 21:23:45 -0300

    componentes
    Aula de componentes devmidia

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import Header from './componentes/Header/index';
+import Footer from './componentes/Footer/footer';
+import HomePage from './componentes/HomePage/index';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <HomePage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
